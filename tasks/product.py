bfd782ae49984d08eb6390b085b2539c3a65e936
Andrey.K <andrey-8991@yandex.ru>
2023-02-13 19:03:21 +0300

    solve tasks

--- tasks/product.py
+++ tasks/product.py
@@ -10,6 +10,6 @@ def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
     Должна возвращать все пары элементы двух массивов:
     cartesian_product([1, 2], [3, 4]) == [(1, 3), (1, 4), (2, 3), (2, 4)]
     """
-    res = [(i, j) for i in arr1 for j in arr2]
-    return res
+    result = [(i, j) for i in arr1 for j in arr2]
+    return result
 
